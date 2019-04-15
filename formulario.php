49d3ae202fab23d6380a824778070c090e4c79d4
Hector Alejandro Sandoval Saucedo <alxsan@outlook.com>
2019-04-15 11:21:30 -0500

    formulario.php
    Simple formato para formulario.

---
+++ formulario.php
@@ -0,0 +1,34 @@
+<form class=" " id=" " action=" " method=" ">
+						<div class=" ">
+							<h2>Acceso</h2>
+							
+						</div>
+<!-- --------------------------------------------------------------------------------------------------------------- -->
+						<div class=" ">
+							
+							<input type="text" placeholder="Usuario" id="usuario" class=" " name="usuario" required>
+              
+						</div>
+<!-- --------------------------------------------------------------------------------------------------------------- -->
+						<div class=" ">
+            
+							
+							<input type="text" placeholder="Email" id="email" class="form-control" name="email" required>
+              
+						</div>
+<!-- --------------------------------------------------------------------------------------------------------------- -->
+						<div class=" ">
+            
+							
+							<input type="password" placeholder="Clave" id="clave" class="form-control" name="clave" required>
+              
+						</div>
+<!-- --------------------------------------------------------------------------------------------------------------- -->
+						<div class=" ">
+							
+							<div class=" ">
+								<button id="enviar" class=" " type="submit" name="submit">Salvar</button>
+							</div>
+						</div>
+				
+					</form>
