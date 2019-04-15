d312b96453c6ac1b9d0173563ca0dc1f3464b6e4
Hector Alejandro Sandoval Saucedo <alxsan@outlook.com>
2019-04-15 11:16:26 -0500

    Create conexion.php
    En la conexión con la BBDD se crean variables donde guardamos la información del servidor, la BBDD, usuario de la BBDD y su contraseña, con el objetivo de facilitar el manejo de los datos. Este se crea en un archivo separado, para después ser incluido mediante un INCLUDE_ONCE en los archivos donde sea necesario.

---
+++ conexion.php
@@ -0,0 +1,8 @@
+<?php 
+
+	//DATOS DE LA BBDD PARA INICIAR LA CONEXIÓN CON LA BBDD
+
+	$bd_host="localhost"; //nombre del servidor donde se encuentra la BBDD.
+	$bd_name="psicosmart";//nombre de la BBDD.
+	$bd_user="root"; //nombre del usuario que tiene acceso a la BBDD.
+	$bd_pass=""; //password de la BBDD del usuario.
